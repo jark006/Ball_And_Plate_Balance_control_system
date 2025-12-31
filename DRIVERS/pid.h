344d534422c175b1416be56cfbfe9897b1a991eb
JARK006 <jark006@qq.com>
2026-01-01 05:27:24 +0800

    update

--- DRIVERS/pid.h
+++ DRIVERS/pid.h
@@ -4,21 +4,19 @@
 #include "stdint.h"
 #include "stm32f10x.h"
 
-typedef struct _POINT {
+typedef struct {
     uint16_t last;   // 上次坐标
     uint16_t now;    // 当前坐标
     uint16_t target; // 目标坐标
-
-    uint16_t p[10]; // idx:0 首位预留， idx: 1~9 对应 p1~p9标识点
+    uint16_t p[10]; // idx:0 首位预留， idx: 1 ~ 9 对应 p1 ~ p9标识点
 
     int angle; // 舵机角度偏值
     int anglewrite;
     int speed;
     int speedfb;
-
 } Coordinate; // 坐标结构体
 
-typedef struct _pid {
+typedef struct {
     float SetValue;    // 目标值
     float ActualValue; // 当前值
     float err;         // 偏差值
